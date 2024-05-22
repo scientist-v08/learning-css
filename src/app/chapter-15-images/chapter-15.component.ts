73d4bc2ae3976e1af98448569acfc9a352938103
Manoj.V <manoj.v@avaali.com>
2024-05-22 11:09:01 +0530

    [Task] Chapter-15 images

--- src/app/chapter-15-images/chapter-15.component.ts
+++ src/app/chapter-15-images/chapter-15.component.ts
@@ -14,10 +14,25 @@ import { Component } from "@angular/core";
       height="200"
     >
   </section> -->
-  <section class="hero">
-    <figure class="profile-pic-figure">
-      
-    </figure>
+  <!-- <div class="container">
+    <section class="hero">
+      <figure class="profile-pic-figure">
+        <img
+          ngSrc="../../assets/profile-800x800.png"
+          alt="Profile picture"
+          title="Profile picture"
+          height="400"
+          width="400">
+        <figcaption class="offscreen">Jane Doe</figcaption>
+      </figure>
+      <h1 class="h1">
+        <span class="nowrap">Hello 👋</span>
+        <span class="nowrap">I'm Jane</span>
+      </h1>
+    </section>
+  </div> -->
+  <section class="container2">
+    <p class="clip">Jane</p>
   </section>
   `,
   styleUrl:'./chapter-15.component.css'
